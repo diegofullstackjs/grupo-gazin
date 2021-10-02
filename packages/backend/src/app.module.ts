b1228da8c777b0013d21a5576998fddb1666c9c1
Kelvin Diego da Silva Soares <kelvin.soares@meta.com.br>
2021-10-02 09:47:50 -0300

    Refatorando codigo pegando informações em variaveis de ambiente

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DevelopersModule } from './developers/developers.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
+      host: String(process.env.DB_HOST),
       port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      username: String(process.env.DB_USERNAME),
+      password: String(process.env.DB_PASSWORD),
+      database: String(process.env.DB_DATABASE),
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
-    }),
+      synchronize: true,
+    } as TypeOrmModuleOptions),
     DevelopersModule,
   ],
   controllers: [],
